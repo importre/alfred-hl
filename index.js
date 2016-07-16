4f0fc9943aa7c55341d0606a72b353ca1da5add6
Jaewe Heo <jaeweheo@gmail.com>
2016-07-17 06:22:05 +0900

    remove comp

--- index.js
+++ index.js
@@ -5,25 +5,10 @@ const utils = require('./utils');
 const input = alfy.input.toLowerCase();
 const languagesDir = './node_modules/highlight.js/lib/languages';
 
-function comp(a, b) {
-  const i = a.indexOf(input);
-  const j = b.indexOf(input);
-  if (i >= 0 && j >= 0) {
-    if (i - j === 0) {
-      if (a.length === b.length) return a.localeCompare(b);
-      return a.length - b.length;
-    }
-    return i - j;
-  }
-  if (i >= 0) return -1;
-  if (j >= 0) return 1;
-  return a.localeCompare(b);
-}
-
 const output = fs
   .readdirSync(languagesDir)
   .filter(lang => lang.endsWith('.js') && lang.indexOf(input) >= 0)
-  .sort(comp)
+  .sort()
   .map(lang => {
     lang = lang.replace('.js', '');
     return {
--- theme.js
+++ theme.js
@@ -4,25 +4,10 @@ const alfy = require('alfy');
 const input = alfy.input.toLowerCase();
 const themeDir = './node_modules/highlight.js/styles/';
 
-function comp(a, b) {
-  const i = a.indexOf(input);
-  const j = b.indexOf(input);
-  if (i >= 0 && j >= 0) {
-    if (i - j === 0) {
-      if (a.length === b.length) return a.localeCompare(b);
-      return a.length - b.length;
-    }
-    return i - j;
-  }
-  if (i >= 0) return -1;
-  if (j >= 0) return 1;
-  return a.localeCompare(b);
-}
-
 const output = fs
   .readdirSync(themeDir)
   .filter(theme => theme.endsWith('.css') && theme.indexOf(input) >= 0)
-  .sort(comp)
+  .sort()
   .map(theme => {
     theme = theme.replace('.css', '');
     return {
